f5a188233ffde2b3477837c92957a8567d84153f
vikram-krishnamoorthy <43487485+vikram-krishnamoorthy@users.noreply.github.com>
2021-06-12 18:44:35 -0400

    create CRUD blog app functionality

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 
-const app = express()
+const postRouter = require("./routes/postRoutes");
+
+const app = express();
+
+const mongo_url = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectMongoWithRetry = () => {
+    mongoose
+    .connect(mongo_url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log("Sucessfully connected to DB!"))
+    .catch((e) => {
+        console.log(e);
+        setTimeout(connectMongoWithRetry, 5000)    
+    });
+};
+
+connectMongoWithRetry();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("<h2>Hi There!!</h2>")
 });
+
+app.use("/api/v1/posts", postRouter);
 
 const port = process.env.PORT || 3000;
 
